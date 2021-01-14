d666314d82cae021baaeea1588e9ebdc21a357db
Daniel Douek <daniel@swimm.io>
2021-01-14 16:01:16 +0200

    autosyncable2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,6 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/pexels-miles-hardacre-2404370.jpg')"
-          class="my-3"
-          contain
-          height="600"
-        />
       </v-col>
 
       <v-col class="mb-4">
