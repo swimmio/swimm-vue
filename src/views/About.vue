7e9fa57295062724433e98b766b2bb16e16f427e
giladax <giladkoch1@gmail.com>
2022-04-26 15:57:00 +0300

    moshe

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,17 @@
 <template>
   <v-container>
+  <HelloWorld/>
     <h1>This is an about page</h1>
   </v-container>
 </template>
 
 <script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
 export default {
   name: "About",
   components: {
+    HelloWorld
   },
   computed: {
   },
