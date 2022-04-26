9143eb53e8676b55ab0b7488fb29013d9419ee8e
giladax <giladkoch1@gmail.com>
2022-04-26 15:27:57 +0300

    Swimm Unit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,15 @@
 <template>
   <v-container>
     <h1>This is an about page</h1>
-    <h2>{{ aboutPage }}</h2>
-    <hello-world />
   </v-container>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "About",
   components: {
-    HelloWorld,
   },
   computed: {
-    aboutPage() {
-      return `Today is ${(new Date).toString()}`;
-    },
   },
 };
 </script>
